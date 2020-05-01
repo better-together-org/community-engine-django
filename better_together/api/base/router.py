d5f317ebe35c21cef48e4fde4a8d8c6f448cc7f5
Robert Smith <rsmithlal@gmail.com>
2020-04-30 22:56:11 -0400

    Added redirects for base api paths

--- better_together/api/base/router.py
+++ better_together/api/base/router.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 from rest_framework.routers import APIRootView, DefaultRouter, SimpleRouter
 
 from . import views
@@ -29,5 +30,7 @@ router.register(r'invitations', views.InvitationViewSet)
 app_name = 'better_together_api'
 
 api_urlpatterns = [
+    path('bt/', RedirectView.as_view(url='api/v1', permanent=False), name='bt_redirect'),
+    path('bt/api/', RedirectView.as_view(url='v1', permanent=False), name='bt_api_redirect'),
     path('bt/api/v1/', include(router.urls), name=app_name + '_v1')
 ]
