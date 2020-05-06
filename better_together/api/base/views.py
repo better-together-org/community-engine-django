d56edbf7528bf143ed4fda22f0751f39048056ad
Robert Smith <rsmithlal@gmail.com>
2020-05-05 23:33:28 -0400

    removed unused import

--- better_together/api/base/views.py
+++ better_together/api/base/views.py
@@ -4,7 +4,6 @@ from better_together import models
 from django.contrib.auth import get_user_model
 from rest_framework import permissions, status
 from rest_framework.decorators import action
-from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin, CreateModelMixin
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
